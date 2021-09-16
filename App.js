6701324de7b7f3f002405bbb6e888ba7cd67bc79
tldnr1 <tldnr1@knu.ac.kr>
2021-09-17 01:16:31 +0900

    second commit

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello React Native</Text>
+      <TouchableOpacity onPress={() => console.log("Image pressed")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -12,8 +27,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: "#FFF",
-    // alignItems: "center",
+    backgroundColor: "#FFF",
+    justifyContent: "center",
+    alignItems: "center",
     marginVertical: 20,
     marginHorizontal: 10,
   },
